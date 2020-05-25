3f3fd9c9b69d3ef50df998b71c7cbd7b991b47c9
simplebunsen <really@dontdoxme.com>
2020-05-25 12:27:39 +0200

    Initial commit, barebones setup of the workspace

---
+++ src/main/java/simplebunsen/mentalblocks/MentalBlocks.java
@@ -0,0 +1,41 @@
+package simplebunsen.mentalblocks;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("mentalblocks")
+public class MentalBlocks
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public MentalBlocks() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event)
+    {
+
+    }
+}
